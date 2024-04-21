effcf28ff4e01517275ecf4c5595bac5d3b3af2d
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-04-21 20:08:31 +0200

    refactor: Author model

---
+++ src/models/Author.ts
@@ -0,0 +1,11 @@
+class Author {
+    name: string;
+    desc: string;
+    image: string;
+
+    constructor(name: string, desc: string, image: string,) {
+        this.name = name;
+        this.desc = desc;
+        this.image = image;
+    }
+};
