2949219b1800a833884c39770651a480613c1383
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-04-24 13:13:15 +0200

    feat/style:ResponeFactory, roleChecker & formmating

---
+++ src/middlewares/roleChecker.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import ResponseFactory from "../factories/ResponseFactroy";
+
+type UserToken = {
+  name: String;
+  role: String;
+};
+type UseredRequest = Request & { user: UserToken };
+type RoleParser = (
+  req: UseredRequest,
+  res: Response,
+  next: NextFunction
+) => void;
+type AllowList = String[];
+
+// whitelist for allowed roles to access an endpoint
+// blacklist to ban specific role from using an endpoint
+export default function roleChecker(
+  whitelist: AllowList = [],
+  blacklist: AllowList = []
+): RoleParser {
+  return (req: UseredRequest, res: Response, next: NextFunction) => {
+    try {
+      let userRole = req.user?.role;
+      if (blacklist.includes(userRole) || !whitelist.length) {
+        const status = 401;
+        return res
+          .status(status)
+          .json({ ...ResponseFactory.create(status, "Unauthorized B**ch") });
+      }
+      if (whitelist.includes(userRole)) {
+        next();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
