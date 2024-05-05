1580f03b961eaa4f801569620307f87fde451c22
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2024-05-05 23:10:06 +0300

    feat/style:using ejs as express view template/formmating change

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
-import express, { Express, Request, Response } from 'express'
-import morgan from 'morgan'
-import fs from 'fs'
-import cors from 'cors'
-import vars from './configs/env'
-import router from './routes/router'
-import path from 'path'
+import express, { Express, Request, Response } from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
+import cors from 'cors';
+import vars from './configs/env';
+import router from './routes/router';
+import path from 'path';
 
-const app: Express = express()
-const port: number = vars.port as number
+const app: Express = express();
+const port: number = vars.port as number;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors({ origin: vars.origins }))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: vars.origins }));
 app.use(
   morgan('common', {
     stream: fs.createWriteStream(
@@ -19,25 +20,25 @@ app.use(
       { flags: 'a' },
     ),
   }),
-)
-app.use(morgan('dev'))
-app.use(router)
+);
+app.use(morgan('dev'));
+app.use(router);
 
 app.use((req: Request, res: Response) =>
   res.status(404).json({ success: false, message: 'Not Found' }),
-)
+);
 
 const server = app.listen(port, () =>
   console.log(`app running on port ${port}`),
-)
+);
 
 // add it to seprate file
 process.on('SIGINT', () => {
-  console.log('Received SIGINT. Closing server...')
+  console.log('Received SIGINT. Closing server...');
   server.close(() => {
-    console.log('Server closed.')
-    process.exit(0)
-  })
-})
+    console.log('Server closed.');
+    process.exit(0);
+  });
+});
 
-export default app
+export default app;
